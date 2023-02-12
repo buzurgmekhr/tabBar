054c7d0dc8931679f55e71d2ab0bd430e21b0ae7
buzurgmexr <buzurgmexrsultonaliyev@gmail.com>
2023-02-12 12:45:45 +0500

    customize label

--- tabBar/ViewController.swift
+++ tabBar/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
   var label: UILabel {
     let label = UILabel()
+    label.text = "Initial label text"
+    label.textColor = .orange
+    label.backgroundColor = .black
+    label.font = .systemFont(ofSize: 32)
     
     return label
   }()
