2ae4f0b93a705c3e0d903595953e3089b81462d1
buzurgmexr <buzurgmexrsultonaliyev@gmail.com>
2023-02-12 12:35:28 +0500

    initial commit

---
+++ tabBar/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  tabBar
+//
+//  Created by Sultonaliyev Buzurgmexr on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
